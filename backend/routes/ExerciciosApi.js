0c90518913978b29363970f1c472dc7774aae95b
KarlosHenryque <karloshenryquep@gmail.com>
2025-09-06 18:51:42 -0300

    Tela com treinos via Json

--- backend/routes/ExerciciosApi.js
+++ backend/routes/ExerciciosApi.js
@@ -1,8 +1,19 @@
 const express = require('express');
 const router = express.Router();
+const path = require('path');
+const fs = require('fs');
 
-router.get('/', async (req, res) => {
+router.get('/', (req, res) => {
+  const filePath = path.join(__dirname, '../json/exercises.json');
 
+  fs.readFile(filePath, 'utf8', (err, data) => {
+    if (err) {
+      return res.status(500).json({ error: 'Erro ao ler o arquivo' });
+    }
+
+    const exercises = JSON.parse(data);
+    res.json(exercises);
+  });
 });
 
 module.exports = router;
